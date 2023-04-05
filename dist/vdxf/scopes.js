f659fa32f8d6aaa9618f7871b2ecfacef199009f
Chris <34682781+monkins1010@users.noreply.github.com>
2023-04-05 09:25:11 +0100

    Added Agreement Scope

--- dist/vdxf/scopes.js
+++ dist/vdxf/scopes.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.IDENTITY_VIEW = void 0;
+exports.IDENTITY_AGREEMENT = exports.IDENTITY_VIEW = void 0;
 exports.IDENTITY_VIEW = {
     vdxfid: "iLUrA89mDKnwxZcMiPadfNB9TLp58A2TKU",
     hash160result: "aeab47faa1b2bde2633a63b8284770a8e5c489ba",
@@ -8,4 +8,12 @@ exports.IDENTITY_VIEW = {
         namespace: "i5w5MuNik5NtLcYmNzcvaoixooEebB6MGV",
         name: "vrsc::identity.authentication.permission.read",
     },
+};
+exports.IDENTITY_AGREEMENT = {
+    vdxfid: "i3fMEsmYzGbd8s7EM9uKZ28fUf9LZAMnEe",
+    hash160result: "894dae7e135484ccfad8924ad59dec9619110c02",
+    qualifiedname: {
+        namespace: "i5w5MuNik5NtLcYmNzcvaoixooEebB6MGV",
+        name: "vrsc::identity.authentication.permission.agree"
+    }
 };
--- src/vdxf/scopes.ts
+++ src/vdxf/scopes.ts
@@ -8,3 +8,12 @@ export const IDENTITY_VIEW: VDXFKeyInterface = {
     name: "vrsc::identity.authentication.permission.read",
   },
 };
+
+export const IDENTITY_AGREEMENT: VDXFKeyInterface = {
+  vdxfid: "i3fMEsmYzGbd8s7EM9uKZ28fUf9LZAMnEe",
+  hash160result: "894dae7e135484ccfad8924ad59dec9619110c02",
+  qualifiedname: {
+    namespace: "i5w5MuNik5NtLcYmNzcvaoixooEebB6MGV",
+    name: "vrsc::identity.authentication.permission.agree"
+  }
+};
